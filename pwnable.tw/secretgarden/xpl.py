5d020d5cb33d550a9c8de6f04e6b0b1394b78522
intranautic <intranautic@gmail.com>
2023-04-25 22:07:14 -0700

    new

--- pwnable.tw/secretgarden/xpl.py
+++ pwnable.tw/secretgarden/xpl.py
@@ -19,7 +19,8 @@ libc = ELF('libc_64.so.6', checksec=0)
 if len(argv) >= 2 and argv[1] == '-r':
   p = remote('chall.pwnable.tw', 10203)
 else:
-  p = process([lib_path+'/ld-2.23.so', './secretgarden'],env={'LD_PRELOAD':'libc_64.so.6'})
+  p = process([lib_path+'/ld-2.23.so', './secretgarden'], \
+    env={'LD_PRELOAD':'libc_64.so.6'})
 s = lambda x, r="" : \
   p.sendafter(r, x) if r else p.send(x)
 sl = lambda x, r="" : \
