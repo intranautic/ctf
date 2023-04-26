f1abbfdb88c011e06c8e15e916d9dc429a9bf838
intranautic <intranautic@gmail.com>
2023-04-26 13:10:44 +0800

    chjange

--- pwnable.tw/tcache_tear/xpl.py
+++ pwnable.tw/tcache_tear/xpl.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 from pwn import *
 from sys import argv
-binary = ELF()
-libc = ELF()
+lib_path="/glibc/2.27/64/lib"
+binary = ELF('tcache_tear', checksec=0)
+libc = ELF('libc-2.27.so', checksec=0)
 if len(argv) >= 2 and argv[1] == '-r':
-  p = remote()
+  p = remote('chall.pwnable.tw', 10207)
 else:
-  p = binary.process(env={})
+  p = process([lib_path+'/ld-2.27.so', './tcache_tear'], \
+    env={'LD_PRELOAD':'libc-2.27.so'})
 s = lambda x, r="" : \
   p.sendafter(r, x) if r else p.send(x)
 sl = lambda x, r="" : \
