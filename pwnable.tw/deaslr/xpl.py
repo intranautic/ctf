9ac9b9f6a4e5a45b39c07948453875c1900c44f5
intranautic <intranautic@gmail.com>
2023-05-01 21:41:14 -0700

    more

---
+++ pwnable.tw/deaslr/xpl.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+from pwn import *
+from sys import argv
+binary = ELF('deaslr', checksec=0)
+libc = ELF('libc_64.so.6', checksec=0)
+if len(argv) >= 2 and argv[1] == '-r':
+  p = remote('chall.pwnable.tw', 10402)
+else:
+  p = binary.process(env={})
+s = lambda x, r="" : \
+  p.sendafter(r, x) if r else p.send(x)
+sl = lambda x, r="" : \
+  p.sendlineafter(r, x) if r else p.sendline(x)
+
+p.interactive()
+
--- study/dynamic-pin/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-if [ $# -eq 0 ]
-  then
-    echo "Usage: ./build.sh <path to pin root>"
-    exit 1
-fi
-make PIN_ROOT=$1
