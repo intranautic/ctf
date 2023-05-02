aceeed73884a418a310a1f3be78b2bd6c5d83856
intranautic <intranautic@gmail.com>
2023-05-02 14:06:00 -0700

    pin

---
+++ study/dynamic-pin/count.cpp
@@ -0,0 +1,31 @@
+// counts the number of instructions process executes
+#include <stdio.h>
+#include "pin.H"
+
+KNOB<std::string> log_file(KNOB_MODE_WRITEONCE, "pintool", "o", "count.log", "output file name");
+
+unsigned long count = 0;
+
+int main(int argc, char** argv) {
+  PIN_InitSymbols();
+  if (PIN_Init(argc, argv)) {
+    fprintf(stderr, "This tool counts the number of instructions the binary executes\n%s",
+      KNOB_BASE::StringKnobSummary().c_str());
+    return -1;
+  }
+
+  PIN_SetSyntaxIntel();
+
+  INS_AddInstrumentFunction([](INS i, void* v) -> void {
+    // instrument a call before every instruction via IPOINT_BEFORE
+    INS_InsertCall(i, IPOINT_BEFORE, (AFUNPTR)[](){++count;}, IARG_END);
+  }, 0);
+
+  PIN_AddFiniFunction([](int c, void* v){
+    fprintf(fopen(log_file.Value().c_str(), "w"),
+      "Instruction Count: %lu\n", count);
+  }, 0);
+
+  PIN_StartProgram();
+  return 0;
+}
--- study/dynamic-pin/profile.cpp
+++ study/dynamic-pin/profile.cpp
@@ -1,32 +1,10 @@
-// profile execution of process
-#include <stdio.h>
+// pin tool which profiles the process
 #include "pin.H"
 
-KNOB<std::string> log_file(KNOB_MODE_WRITEONCE, "pintool", "o", "count.out", "output file name");
-
-unsigned long count = 0;
+KNOB<std::string> log_file(KNOB_MODE_WRITEONCE, "pintool", "o", "profile.log", "output file");
 
 
 int main(int argc, char** argv) {
-  PIN_InitSymbols();
-  if (PIN_Init(argc, argv)) {
-    fprintf(stderr, "This tool profiles the binary\n%s",
-      KNOB_BASE::StringKnobSummary().c_str());
-    return -1;
-  }
 
-  PIN_SetSyntaxIntel();
-
-  INS_AddInstrumentFunction([](INS i, void* v) -> void {
-    // instrument a call before every instruction via IPOINT_BEFORE
-    INS_InsertCall(i, IPOINT_BEFORE, (AFUNPTR)[](){++count;}, IARG_END);
-  }, 0);
-
-  PIN_AddFiniFunction([](int c, void* v){
-    fprintf(fopen(log_file.Value().c_str(), "w"),
-      "Instruction Count: %lu\n", count);
-  }, 0);
-
-  PIN_StartProgram();
   return 0;
 }
