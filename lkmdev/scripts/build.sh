c8447a98208733c3aa8bb4395043e99ab0adbc14
intranautic <intranautic@gmail.com>
2024-03-04 17:35:44 -0800

    sorrya bout that i didnt mean it you know i love you right?

--- lkmdev/scripts/build.sh
+++ lkmdev/scripts/build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-if [ $# -ne 2 ]; then
-  printf "Usage: %s <rootfs> <initramfs>\n" $0
+if [ $# -ne 1 ]; then
+  printf "Usage: %s <rootfs>\n" $0
   exit
 fi
 if [ ! -d $1 ]; then
@@ -8,5 +8,5 @@ if [ ! -d $1 ]; then
   exit
 fi
 
-cd $1
-find . -print0 | cpio --null -ov --format=newc>$2 && gzip -f $2
+(cd $1 && find . -print0 | cpio --null -ov --format=newc >../initramfs.cpio)
+gzip -f initramfs.cpio
